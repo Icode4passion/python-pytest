2aa2dc2bd462acbadbac022deca218ba00833c1c
Icode4passion <39987888+Icode4passion@users.noreply.github.com>
2025-03-17 21:53:47 +0530

    Create test_cal.py

---
+++ test_cal.py
@@ -0,0 +1,13 @@
+from cal import *
+
+def test_add(x,y):
+  assert add(2,3)==5
+
+def test_subtract(x,y):
+  assert subtract(3-1)==2
+
+def test_multiply(x,y):
+  assert multiply(2,3)==6
+
+def test_divide(x,y):
+  assert divide(2,2)==1
